45b42f3abd134ce266a5c56faea644d2b1aa6057
Owner <me7445950@gmail.com>
2024-02-28 15:25:45 -0800

    add some feature and count comments

--- products/views.py
+++ products/views.py
@@ -24,4 +24,6 @@ class Detail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(Detail, self).get_context_data(**kwargs)
         context["forms"] = Db_contact.objects.all()
+        context["number"] = Db_contact.objects.count()
+
         return context
